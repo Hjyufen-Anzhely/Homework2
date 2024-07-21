cb23dd133efc08691b6e1bea764b788ea7585985
Hjyufen-Anzhely <anzh.zimina@yandex.ru>
2024-07-21 17:35:49 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
 number_of_completed_hw = 12
 number_of_hours_spent = 1.5
 name_course = "Python"
-time_per_task = 1.5/12
+time_per_task = number_of_hours_spent/number_of_completed_hw
 print("Курс:",name_course, "всего задач:",number_of_completed_hw,"затрачено часов:",number_of_hours_spent,"среднее время выполнения",time_per_task,)
